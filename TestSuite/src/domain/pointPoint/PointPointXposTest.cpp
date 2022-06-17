5a72e74dbf1531cc3c1d965816bc76fccde5cb8e
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2022-06-17 10:39:10 +0100

    PointPointXposTest successes function defined

--- TestSuite/src/domain/pointPoint/PointPointXposTest.cpp
+++ TestSuite/src/domain/pointPoint/PointPointXposTest.cpp
@@ -1,7 +1,7 @@
 #include "PointPointXposTest.h"
 
 PointPointXposTest::PointPointXposTest()
-	: itterations{ 4 }, successes{ 0 }
+	: itterations{ 4 }
 {
 }
 
@@ -11,6 +11,18 @@ PointPointXposTest::~PointPointXposTest()
 
 std::string PointPointXposTest::test()
 {
+	if (successes() == itterations)
+	{
+		return std::string();
+	}
+
+	return "point point xpos test failed\n";
+}
+
+int PointPointXposTest::successes()
+{
+	int output = 0;
+
 	for (int i{ 0 }; i < itterations; i++)
 	{
 		float pointA[3]{
@@ -27,13 +39,8 @@ std::string PointPointXposTest::test()
 
 		collision3D::PointPoint unit;
 		unit.calculate(pointA, pointB);
-		successes += unit.getCollision() == false;
+		output += unit.getCollision() == false;
 	}
 
-	if (successes == itterations)
-	{
-		return std::string();
-	}
-
-	return "point point xpos test failed\n";
+	return output;
 }
