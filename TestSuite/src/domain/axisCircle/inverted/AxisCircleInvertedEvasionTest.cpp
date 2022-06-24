beefd0c2369a20a4feeae4ecc916b17068396f59
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2022-06-24 14:59:00 +0100

    AxisCircleInvertedEvasionTest collision function defined

--- TestSuite/src/domain/axisCircle/inverted/AxisCircleInvertedEvasionTest.cpp
+++ TestSuite/src/domain/axisCircle/inverted/AxisCircleInvertedEvasionTest.cpp
@@ -21,50 +21,55 @@ std::string AxisCircleInvertedEvasionTest::test()
 
 int AxisCircleInvertedEvasionTest::successes()
 {
-	int successes = 1;
+	int output = 1;
 
 	for (int i{ 1 }; i < itterations; i++)
 	{
-		float axis[6]{
-			(float)i,
-			(float)i,
-			(float)i,
-			0.0f,
-			0.0f,
-			0.0f
-		};
-
-		float circle[4]{
-			-(float)i,
-			-(float)i,
-			-(float)i,
-			0.0f
-		};
-
-		float distanceX = axis[0] - circle[0];
-		distanceX *= distanceX;
-
-		float distanceY = axis[1] - circle[1];
-		distanceY *= distanceY;
-
-		float distanceZ = axis[2] - circle[2];
-		distanceZ *= distanceZ;
-
-		float input = distanceX + distanceY + distanceZ;
-		float output = input;
-
-		while ((output - input / output) > 0.000001f)
-		{
-			output = (output + input / output) / 2;
-		}
-
-		circle[3] = -output + 0.1f;
-
-		collision3D::AxisCircle unit;
-		unit.calculate(axis, circle);
-
-		successes += !unit.getCollision();
+		output += !collision((float)i);
 	}
 
-	return successes;
+	return output;
+}
+
+bool AxisCircleInvertedEvasionTest::collision(float input)
+{
+	float axis[6]{
+		input,
+		input,
+		input,
+		0.0f,
+		0.0f,
+		0.0f
+	};
+
+	float circle[4]{
+		-input,
+		-input,
+		-input,
+		0.0f
+	};
+
+	float distanceX = axis[0] - circle[0];
+	distanceX *= distanceX;
+
+	float distanceY = axis[1] - circle[1];
+	distanceY *= distanceY;
+
+	float distanceZ = axis[2] - circle[2];
+	distanceZ *= distanceZ;
+
+	float temp = distanceX + distanceY + distanceZ;
+	float squereRoot = temp;
+
+	while ((squereRoot - temp / squereRoot) > 0.000001f)
+	{
+		squereRoot = (squereRoot + temp / squereRoot) / 2;
+	}
+
+	circle[3] = -squereRoot + 0.1f;
+
+	collision3D::AxisCircle unit;
+	unit.calculate(axis, circle);
+
+	return unit.getCollision();
 }
