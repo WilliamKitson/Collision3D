74d38c46ffc2c178452bfe71f533765e9cae193a
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2022-06-24 13:41:31 +0100

    CircleCircleInvertedEvasionTest collision function defined

--- TestSuite/src/domain/circleCircle/inverted/CircleCircleInvertedEvasionTest.cpp
+++ TestSuite/src/domain/circleCircle/inverted/CircleCircleInvertedEvasionTest.cpp
@@ -21,52 +21,57 @@ std::string CircleCircleInvertedEvasionTest::test()
 
 int CircleCircleInvertedEvasionTest::successes()
 {
-	int successes = 1;
+	int output = 1;
 
 	for (int i{ 1 }; i < itterations; i++)
 	{
-		float a[4]{
-			(float)i,
-			(float)i,
-			(float)i,
-			0.0f
-		};
-
-		float b[4]{
-			-(float)i,
-			-(float)i,
-			-(float)i,
-			0.0f
-		};
-
-		float distanceX = a[0] - b[0];
-		distanceX *= distanceX;
-
-		float distanceY = a[1] - b[1];
-		distanceY *= distanceY;
-
-		float distanceZ = a[2] - b[2];
-		distanceZ *= distanceZ;
-
-		float input = distanceX + distanceY + distanceZ;
-		float output = input;
-
-		while ((output - input / output) > 0.000001f)
-		{
-			output = (output + input / output) / 2;
-		}
-
-		output *= 0.5f;
-		output -= 0.1f;
-
-		a[3] = -output;
-		b[3] = -output;
-
-		collision3D::CircleCircle unit;
-		unit.calculate(a, b);
-
-		successes += !unit.getCollision();
+		output += !collision((float)i);;
 	}
 
-	return successes;
+	return output;
+}
+
+bool CircleCircleInvertedEvasionTest::collision(float input)
+{
+	float a[4]{
+		input,
+		input,
+		input,
+		0.0f
+	};
+
+	float b[4]{
+		-input,
+		-input,
+		-input,
+		0.0f
+	};
+
+	float distanceX = a[0] - b[0];
+	distanceX *= distanceX;
+
+	float distanceY = a[1] - b[1];
+	distanceY *= distanceY;
+
+	float distanceZ = a[2] - b[2];
+	distanceZ *= distanceZ;
+
+	float temp = distanceX + distanceY + distanceZ;
+	float output = temp;
+
+	while ((output - temp / output) > 0.000001f)
+	{
+		output = (output + temp / output) / 2;
+	}
+
+	output *= 0.5f;
+	output -= 0.1f;
+
+	a[3] = -output;
+	b[3] = -output;
+
+	collision3D::CircleCircle unit;
+	unit.calculate(a, b);
+
+	return unit.getCollision();
 }
