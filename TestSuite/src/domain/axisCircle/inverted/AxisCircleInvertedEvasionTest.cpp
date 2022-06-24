347d6ba5be9277022354dc6c1bde5b033254819e
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2022-06-24 15:01:42 +0100

    AxisCircleInvertedEvasionTest distance function defined

--- TestSuite/src/domain/axisCircle/inverted/AxisCircleInvertedEvasionTest.cpp
+++ TestSuite/src/domain/axisCircle/inverted/AxisCircleInvertedEvasionTest.cpp
@@ -49,16 +49,7 @@ bool AxisCircleInvertedEvasionTest::collision(float input)
 		0.0f
 	};
 
-	float distanceX = axis[0] - circle[0];
-	distanceX *= distanceX;
-
-	float distanceY = axis[1] - circle[1];
-	distanceY *= distanceY;
-
-	float distanceZ = axis[2] - circle[2];
-	distanceZ *= distanceZ;
-
-	float temp = distanceX + distanceY + distanceZ;
+	float temp = distance(axis[0], circle[0]) + distance(axis[1], circle[1]) + distance(axis[2], circle[2]);
 	float squereRoot = temp;
 
 	while ((squereRoot - temp / squereRoot) > 0.000001f)
@@ -72,4 +63,10 @@ bool AxisCircleInvertedEvasionTest::collision(float input)
 	unit.calculate(axis, circle);
 
 	return unit.getCollision();
+}
+
+float AxisCircleInvertedEvasionTest::distance(float a, float b)
+{
+	float output = a - b;
+	return output * output;
 }
